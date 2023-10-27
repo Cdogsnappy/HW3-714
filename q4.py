8151d63f565a94db5841ef08c03b7063f2cd2280
Cdogsnappy <cdogsnappy@gmail.com>
2023-10-27 07:38:31 -0500

    got ADI working

--- q4.py
+++ q4.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 h = 22.5
-P = np.ones((4,4))
+P = np.ones((4, 4))
 b = 5500
-k = (h*h)/(6*b)
+k = (h * h) / (6 * b)
 l = 73
 w = 160
 t = 100
-C = (58,147)
-Q = (58,103)
-T = (31,14)
-fac = [C,Q,T]
+C = (58, 147)
+Q = (58, 103)
+T = (31, 14)
+fac = [C, Q, T]
+
+
 def buildData(f1):
     d = []
     for line in f1:
@@ -20,71 +22,89 @@ def buildData(f1):
         d.append(list1)
     return d
 
+
 def solve():
+    c_conc = []
+    q_conc = []
+    t_conc = []
     f = open('van_vleck.txt', 'r')
     vv = np.array(buildData(f))
-    m,n = vv.shape
-    u = np.zeros(m*n)
+    m, n = vv.shape
+    u = np.zeros(m * n)
     enforce_pizza_zone(u)
-    #build discretization matrix
-    D = np.diag(np.ones(m*n)*-4)
+    # build discretization matrix
+    D = np.diag(np.ones(m * n) * -4)
     for i in range(m):
         for j in range(n):
-            ij = ind(i,j)
-            if in_range(i+1,j):
-                if vv[i+1,j] == 1:
-                    D[ij,ij] += 1
-                else:
-                    D[ij,ind(i+1,j)] = 1
-            if in_range(i,j+1):
-                if vv[i, j+1] == 1:
+            ij = ind(i, j)
+            if in_range(i + 1, j):
+                if vv[i + 1, j] == 1:
                     D[ij, ij] += 1
                 else:
-                    D[ij,ind(i,j+1)] = 1
-            if in_range(i-1,j):
+                    D[ij, ind(i + 1, j)] = 1
+            if in_range(i, j + 1):
+                if vv[i, j + 1] == 1:
+                    D[ij, ij] += 1
+                else:
+                    D[ij, ind(i, j + 1)] = 1
+            if in_range(i - 1, j):
                 if vv[i - 1, j] == 1:
                     D[ij, ij] += 1
                 else:
-                    D[ij,ind(i-1,j)] = 1
-            if in_range(i,j-1):
-                if vv[i, j-1] == 1:
+                    D[ij, ind(i - 1, j)] = 1
+            if in_range(i, j - 1):
+                if vv[i, j - 1] == 1:
                     D[ij, ij] += 1
                 else:
-                    D[ij,ind(i,j-1)] = 1
-    D*=(b/(h*h))
-    times = (1,5,25,100)
+                    D[ij, ind(i, j - 1)] = 1
+    D *= (b / (h * h))
+    times = (1, 5, 25, 100)
     curr = 0
     iter = 0
     while iter < t:
-        u_new = np.dot(D,u)
-        u_new*=k
+        u_new = np.dot(D, u)
+        u_new *= k
         u = u_new + u
         enforce_pizza_zone(u)
-        iter+=k
+        iter += k
+        t_conc.append(u[ind(31, 14)])
+        q_conc.append(u[ind(58, 103)])
+        c_conc.append(u[ind(58, 147)])
         for teach in fac:
             if u[ind(teach[0], teach[1])] > 1e-4:
                 print("Professor at " + str(teach) + " smells pizza! The time is " + str(iter))
                 fac.remove(teach)
         if iter >= times[curr]:
-            uu = np.reshape(np.power(u,.25) , (l,w))
+            uu = np.reshape(np.power(u, .25), (l, w))
             for i in range(l):
                 for j in range(w):
-                    if(vv[i,j] == 1):
-                        uu[i,j] = 0
+                    if (vv[i, j] == 1):
+                        uu[i, j] = 0
             plt.imshow(uu, cmap='hot', interpolation='nearest')
             plt.title("time = " + str(times[curr]))
             plt.show()
-            curr+=1
+            curr += 1
+    plt.plot(np.linspace(0, 100, len(t_conc)), t_conc, label="Professor Q")
+    plt.plot(np.linspace(0, 100, len(t_conc)), q_conc, label="Professor T")
+    plt.plot(np.linspace(0, 100, len(t_conc)), c_conc, label="Professor C")
+    plt.yscale('log')
+    plt.ylim([1e-10, 1])
+    plt.title("Concentrations at Professor's Locations")
+    plt.show()
 
-def in_range(i,j):
-    return i < 73 and j < 160 and i*w + j >= 0
 
-def ind(i,j):
-    return i*w+j
+def in_range(i, j):
+    return i < 73 and j < 160 and i * w + j >= 0
+
+
+def ind(i, j):
+    return i * w + j
+
 
 def enforce_pizza_zone(u):
-    for i in range(36,40):
-        for j in range(44,48):
-            u[ind(i,j)] = 1
+    for i in range(36, 40):
+        for j in range(44, 48):
+            u[ind(i, j)] = 1
+
 
 solve()
--- q5.py
+++ q5.py
@@ -2,42 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-n = 40
-h = 1./n
-dx = 1./n
-dt = .1/n
-mu = dt/(dx*dx)
 
-def uex(x,y):
-    return np.sin(2*np.pi*x)*np.sin(2*np.pi*y)
+def uex(x, y):
+    return np.sin(2 * np.pi * x) * np.sin(2 * np.pi * y)
 
-def solve():
-    #Build tridiagonal
-    D = np.diag(np.ones(n*n)*-2*mu) + np.diag(np.ones(n*n-1)*mu,1) + np.diag(np.ones(n*n-1)*mu,-1)
-    I = np.identity(n*n)
-    u = np.zeros((n*n))
-    #build our vector u representing the 2-D field
-    for i in range(1,n-1):
-        for j in range(1,n-1):
-            u[index(i,j)] = uex(i,j)
+
+def solve(n, times):
+    h = 1. / n
+    dx = 1. / n
+    dt = .1 / n
+    mu = 1 / (dx * dx)
+    # Build tridiagonal
+    D = np.diag(np.ones(n) * -2 * mu) + np.diag(np.ones(n - 1) * mu, 1) + np.diag(np.ones(n - 1) * mu, -1)
+    I = np.identity(n)
+    u = np.zeros((n, n))
+    u_star = np.zeros((n, n))
+    # build our vector u representing the 2-D field
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            u[i, j] = uex(i, j)
     for l in range(n):
-        #step 1
+        A = (I - (dt / 2) * D)
+        # step 1
         for i in range(n):
-            print("")
-        b = np.dot((I + (dt/2)*D),u)
-        A = (I - (dt/2)*D)
-        u_star = np.linalg.solve(A,b)
+            b = u[:, i] + (dt / 2) * np.dot(D, u[:, i])
+            u_star[:, i] = np.linalg.solve(A, b)
 
-        #step 2
-        b_2 = np.dot((I + (dt/2)*D),u_star)
-        u = np.linalg.solve(A,b_2)
-    uu = np.reshape(u,(n,n))
+        # step 2
+        for j in range(n):
+            b_2 = u_star[j, :] + (dt / 2) * np.dot(D, u_star[j, :])
+            u[j, :] = np.linalg.solve(A, b_2)
+        if l in times:
+            fig = plt.figure()
+            ax = plt.axes(projection='3d')
+            X, Y = np.meshgrid(np.linspace(0, 1, n), np.linspace(0, 1, n))
+            ax.plot_surface(X, Y, u, cmap=cm.Blues)
+            plt.title("time = " + str(l + 1) + ", h = " + str(h))
+            plt.show()
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    X,Y = np.meshgrid(np.linspace(0,1,n),np.linspace(0,1,n))
-    ax.plot_surface(X,Y,uu, cmap=cm.Blues)
+    X, Y = np.meshgrid(np.linspace(0, 1, n), np.linspace(0, 1, n))
+    ax.plot_surface(X, Y, u, cmap=cm.Blues)
+    plt.title("time = " + str(l + 1) + ", h = " + str(h))
     plt.show()
 
-def index(i,j):
-    return i*n + j
-solve()
+
+solve(100, (9, 24, 49, 99))
+
+#n_sizes = (40, 80, 160)
+#for n in n_sizes:
+    #solve(n,[39])
